bcf251306cdc1a0e53ac719fca2e7f6cd3b30b0d
Steve Lavoie <steve.l@digitaloperatives.com>
2018-03-31 17:20:29 -0500

    Add GPIO map for reference

--- TankBot/TankBot.ino
+++ TankBot/TankBot.ino
@@ -10,3 +10,11 @@
 
 #include "TankBot.h"
 
+/*********************  Orion Board GPIO Map *********************************
+MePort_Sig mePort[17] = {
+  { NC, NC },{ 11, 10 },{ 3,  9 },{ 12, 13 },{ 8,  2 },
+  { NC, NC },{ A2, A3 },{ A6, A1 },{ A7, A0 },{ 6,  7 },
+  { 5,  4 },{ NC, NC },{ NC, NC },{ NC, NC },{ NC, NC },
+  { NC, NC },{ NC, NC },
+};
+*********************  Orion Board GPIO Map *********************************/
